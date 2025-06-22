17aad2cb9390d29992fab536f76dad13081109ad
Leonardo Ruben Hernandez Silva <leonardoruben2612@gmail.com>
2025-06-22 11:33:15 -0600

    v2.7 Leonardo

--- juego2d/Leonardo/personaje.gd
+++ juego2d/Leonardo/personaje.gd
@@ -11,11 +11,14 @@ func _physics_process(delta):
 	var direccion = Input.get_axis("ui_left","ui_right")
 	velocity.x = direccion * velocidad
 	
-	if not is_on_floor():
+	if not is_on_floor(): 
 		velocity.y += gravedad * delta
 	
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor(): 
 		velocity.y = brinco
 	
 	move_and_slide()
-	
+
+
+func _on_reset_area_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
