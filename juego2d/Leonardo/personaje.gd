9f70fa3e00b6efde2d68f1978a3c525eb2cd2dfb
Leonardo Ruben Hernandez Silva <leonardoruben2612@gmail.com>
2025-06-21 19:40:05 -0600

    v2.4 Leonardo

---
+++ juego2d/Leonardo/personaje.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var velocidad = 200
+var brinco = -400
+var gravedad = 1000
+
+func _ready():
+	add_to_group("jugador")
+
+func _physics_process(delta):
+	var direccion = Input.get_axis("ui_left","ui_right")
+	velocity.x = direccion * velocidad
+	
+	if not is_on_floor():
+		velocity.y += gravedad * delta
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = brinco
+	
+	move_and_slide()
+	
---
+++ juego2d/Leonardo/plataforma.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+enum TipoPlataforma {FIJA, OSCILATORIA, FRAGIL, REBOTE, CAIDA}
+@export var type: TipoPlataforma = TipoPlataforma.FIJA;
+@export var fuerza_rebote := 2.0 
+
+func _ready():
+	actualizar_plataforma()
+	monitorable = true
+	monitoring = true
+	
+func actualizar_plataforma():
+	match type:
+		TipoPlataforma.FIJA:
+			$Sprite2D.modulate = Color. DIM_GRAY
+		TipoPlataforma.OSCILATORIA:
+			$Sprite2D.modulate = Color. REBECCA_PURPLE
+			oscilar()
+		TipoPlataforma.FRAGIL:
+			$Sprite2D.modulate = Color. GHOST_WHITE
+		TipoPlataforma.REBOTE:
+			$Sprite2D.modulate = Color. DARK_ORANGE
+		TipoPlataforma.CAIDA:
+			$Sprite2D.modulate = Color. DARK_ORANGE
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("jugador"):
+		
+		match type: 
+			
+			TipoPlataforma.FRAGIL:
+				await get_tree().create_timer(0.25).timeout
+				queue_free()
+			TipoPlataforma.REBOTE:
+				if body.has_method("puede_rebotar"):
+					body.puede_rebotar(fuerza_rebote)
+				else: 
+					body.velocity.y = body.brinco * fuerza_rebote 
+	
+	
+func oscilar():
+	var tween = create_tween()
+	tween.tween_property(self,"position:x",position.x + 100,2)
+	tween.tween_property(self,"position:y",position.y - 100,2)
+	tween.tween_property(self,"position:x",position.x - 100,2)
+	tween.tween_property(self,"position:y",position.y + 100,2)
+	tween.set_loops() 
