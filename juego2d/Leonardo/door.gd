9df4602b56cab60d63e55b6e9f21c0be92d7243a
Leonardo Ruben Hernandez Silva <leonardoruben2612@gmail.com>
2025-06-23 23:17:50 -0600

    v2.18 Leonardo

--- juego2d/Leonardo/door.gd
+++ juego2d/Leonardo/door.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
 func _ready():
-	# Conectar señal correctamente
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
-func _on_body_entered(body: Node) -> void:
-	print("Entró:", body.name)  # Para probar si funciona
-
-	if body.name == "Personaje":  # Cambia a lo que diga tu jugador
-		var hud = get_node("/root/NIVEL2/HUD")  # Cambia Nivel1 si es necesario
-		hud.get_node("LabelFinal").visible = true
-		hud.get_node("BotonReiniciar").visible = true
+func _on_body_entered(body):
+	if body.name == "Personaje":
+		print("Puerta desbloqueada")
+		var contenedor = get_node("HUD/ContenedorFinal")
+		if contenedor:
+			print("✅ ContenedorFinal encontrado")
+			contenedor.visible = true
+		else:
+			print("❌ No se encontró ContenedorFinal")
--- juego2d/Leonardo/hud.gd
+++ juego2d/Leonardo/hud.gd
@@ -1,3 +1,8 @@
 extends CanvasLayer
+
+
+
+func _on_boton_reiniciar_pressed() -> void:
+	pass # Replace with function body.
 func _on_BotonReiniciar_pressed():
 	get_tree().reload_current_scene()
