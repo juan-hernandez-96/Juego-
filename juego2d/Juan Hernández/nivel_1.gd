ddb1f5fdf0e83e5d7042060172b55ca3cff558cf
Juan Hernandez <juanpablohernandeza2@gmail.com>
2025-06-24 17:22:15 -0700

    v.anillo_movimiento.Juan Hernández

--- juego2d/Juan Hernández/nivel_1.gd
+++ juego2d/Juan Hernández/nivel_1.gd
@@ -1,4 +1,15 @@
 extends Node2D
+var contador_anillos = 0
+@onready var marcador = $HUD/Marcador
+@onready var door = $door  
+
+func sumar_anillo():
+	contador_anillos += 1
+	marcador.text = "Anillos: " + str(contador_anillos)
+
+	if contador_anillos == 5:
+		print("¡Puerta desbloqueada!")
+		door.get_node("CollisionShape2D").disabled = true  # Solo desactiva la colisión
 
 func _ready() -> void:
 	$Label.text = "Si acceder al nivel quieres,"
